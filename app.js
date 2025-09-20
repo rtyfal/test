106ecd97c95b2ef070fb61fd9eeb28f0d7e26c61
rtyfal <qamy10000@gmail.com>
2025-09-20 15:22:02 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require("body-parser")
+const mainRouter = require("./routes/main")
+const userRouter = require("./routes/user")
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+
+app.use("/hello",mainRouter )
+app.use("/users",userRouter)
+
+// app.use((req,res,next,error)=>{
+//     console.log(123)
+//     const message = error.message || "Server error";
+//     const status = error.status || 500
+//     console.log(status, message)
+//     return res.status(status).json({message})
+// })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
